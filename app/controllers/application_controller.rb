38d2cb05baba61fa7194e926abb857c81f7e2829
Rebecca Nesson <rebecca@prx.org>
2012-04-13 13:50:28 -0400

    trying to fix login redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_current_user
   
   def after_sign_in_path_for(resource)
-      redirect_to questions
+      redirect_to questions_path
   end
   
   def set_current_user
